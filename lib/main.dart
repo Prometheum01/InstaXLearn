0c459504bd94de6fccb4d9ffaa731a6241dc664b
Enes Yilmaz <85996021+Prometheum01@users.noreply.github.com>
2024-07-02 22:42:43 +0300

    User Repository and models created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:social_bloc_firebase/app.dart';
 
-void main() {}
+void main() {
+  runApp(const MainApp());
+}
---
+++ packages/user_repository/lib/src/entities/my_user_entity.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+
+class MyUserEntity extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const MyUserEntity({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.picture,
+  });
+
+  Map<String, Object> toDocument() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+      'picture': picture.toString(),
+    };
+  }
+
+  MyUserEntity fromDocument(Map<String, Object> document) {
+    return MyUserEntity(
+      id: document['id'] as String,
+      email: document['email'] as String,
+      name: document['name'] as String,
+      picture: document['picture'] as String,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, picture];
+
+  @override
+  String toString() {
+    return '''
+    User Entity
+    {
+    id: $id
+    email: $email
+    name: $name
+    picture: $picture
+    }
+    ''';
+  }
+}
